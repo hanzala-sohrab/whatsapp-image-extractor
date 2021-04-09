ef7d279d74c412c606c88763da01c3b701dcb22e
hanzala-sohrab <sorb.hanzala@gmail.com>
2021-04-09 22:06:59 +0530

    Resolved bugs in the code

--- client.ts
+++ client.ts
@@ -1,14 +1,13 @@
 import { create, Client, decryptMedia } from "@open-wa/wa-automate";
 
 const axios = require("axios");
-import mime from "mime-types";
+const mime = require("mime-types");
+// const fs = require("fs");
 
 function start(client: Client) {
   client.onMessage(async (message) => {
     if (message.mimetype) {
-      const filename = `${message.t}-${Math.floor(
-        Math.random() * 1000
-      )}.${mime.extension(message.mimetype)}`;
+      const filename = `${message.t}-${Math.floor(Math.random() * 1000)}.${mime.extension(message.mimetype)}`;
       const mediaData = await decryptMedia(message);
       const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
         "base64"
@@ -21,12 +20,19 @@ function start(client: Client) {
         time: message.timestamp,
       };
       await client.sendImage(
-        "919XXXXXXXXX@c.us",
+        "919XXXXXXXX@c.us",
         imageBase64,
         filename,
         `You just received this ${message.type} from *${message.sender.name}* with id ${message.sender.id}, at ${message.timestamp}, in group *${message.chat.formattedTitle}* (id: ${message.chat.id})`
       );
 
+      // Uncomment the below snippet to save the image
+      // fs.writeFile(filename, mediaData, function (err) {
+      //   if (err) {
+      //     return console.log(err);
+      //   }
+      //   console.log("The file was saved!");
+      // });
       const foo = {
         imageURL: imageBase64,
         sender: sender,
