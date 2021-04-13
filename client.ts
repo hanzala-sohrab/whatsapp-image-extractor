6cb7fe638d00475bec5da918247b0fb3b1d7de36
hanzala-sohrab <sorb.hanzala@gmail.com>
2021-04-13 14:18:35 +0530

    Added info for group/personal message

--- client.ts
+++ client.ts
@@ -12,15 +12,15 @@ function start(client: Client) {
       const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
         "base64"
       )}`;
-
       const sender = {
         id: message.sender.id,
         name: message.sender.pushname,
         group: message.chat.formattedTitle,
         time: message.timestamp,
+        isPersonal: message.chatId.includes("c.us"),
       };
       await client.sendImage(
-        "919XXXXXXXX@c.us",
+        "919XXXXXXXXX@c.us",
         imageBase64,
         filename,
         `You just received this ${message.type} from *${message.sender.name}* with id ${message.sender.id}, at ${message.timestamp}, in group *${message.chat.formattedTitle}* (id: ${message.chat.id})`
@@ -51,6 +51,30 @@ function start(client: Client) {
         });
       console.log(JSON.stringify(foo));
     }
+  });
+
+  client.onGlobalParticipantsChanged(async (participantAdded) => {
+    const newUser = {
+      who: participantAdded.who,
+      action: participantAdded.action,
+      group: participantAdded.chat,
+    };
+    await client.sendText(
+      "919XXXXXXXXX@c.us",
+      `who: ${participantAdded.who}\nwhat: ${participantAdded.action}\ngroup: ${participantAdded.chat}`
+    );
+    axios
+      .post("Enter URL here", {
+        newUser: newUser
+      })
+      .then((res) => {
+        console.log(`statusCode: ${res.statusCode}`);
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log(JSON.stringify(newUser));
   });
 }
 
